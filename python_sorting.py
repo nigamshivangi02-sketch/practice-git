678a47bf7bada7dc61daef1ea2a4e505920f0aa6
nigamshivangi02-sketch <nigam.shivangi02@gmail.com>
2025-08-25 15:50:24 +0530

    created a python sorting file

---
+++ python_sorting.py
@@ -0,0 +1,7 @@
+def sort_numbers(numbers):
+    return sorted(numbers)
+
+if __name__ == "__main__":
+    nums = [5, 2, 9, 1, 5, 6]
+    sorted_nums = sort_numbers(nums)
+    print("Sorted numbers:", sorted_nums)
